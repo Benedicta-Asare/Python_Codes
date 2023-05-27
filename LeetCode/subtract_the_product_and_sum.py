f67244618fed4ddc10b6e0825f2ea19422bd6ead
Benedicta <83235057+Benedicta-Asare@users.noreply.github.com>
2023-05-27 21:22:31 +0000

    Create subtract_the_product_and_sum.py

---
+++ LeetCode/subtract_the_product_and_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product = 1
+        add = 0
+        while n > 0:
+            digit = n % 10
+            product *= digit
+            add += digit
+            n //= 10
+        return (product - add)
